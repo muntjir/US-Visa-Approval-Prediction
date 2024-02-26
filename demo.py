93f8eb0e63230d6301ddbf78641f1c3a291f7224
Mohd Muntjir <muntjir.m@gmail.com>
2024-02-26 22:23:15 +0300

    Demo updated

--- demo.py
+++ demo.py
@@ -1,2 +1,10 @@
 from us_visalogger import logging
+from us_visaexception import USvisaException
+import sys
+
+try:
+    a = 1/"1"
+except Exception as e:
+    raise USvisaException (e, sys) from e
+
 logging.info("Welcome to our custom vlogger")
